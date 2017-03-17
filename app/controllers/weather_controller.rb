4ebce401df10f37fda2eacaed81f8ff3b3a5b9dc
David Thomason <dlthomas@us.ibm.com>
2017-03-16 21:27:57 -0400

    Added Weather controller

---
+++ app/controllers/weather_controller.rb
@@ -0,0 +1,10 @@
+class WeatherController < ApplicationController
+  def home
+  end
+
+  def current
+  end
+
+  def forecast
+  end
+end
---
+++ test/controllers/weather_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class WeatherControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get weather_home_url
+    assert_response :success
+  end
+
+  test "should get current" do
+    get weather_current_url
+    assert_response :success
+  end
+
+  test "should get forecast" do
+    get weather_forecast_url
+    assert_response :success
+  end
+
+end
