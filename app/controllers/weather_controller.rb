1dea250126c2199e6eb0aed3e7d6b22111f23ded
David Thomason <dlthomas@us.ibm.com>
2017-03-23 21:00:53 -0400

    Added switched form_for to form_tag Removed weather from strong parameters

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -3,10 +3,17 @@ class WeatherController < ApplicationController
   end
 
   def current
+    location = weather_params[:location]
+    puts location
     response = Weather.new
     @current_weather = response.weather
   end
 
   def forecast
+  end
+
+  private
+  def weather_params
+    params.permit(:location)
   end
 end
